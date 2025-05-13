44299d5e9e02a2b5479d5ed1c2dd9982bb591369
김민정 <alswjd35791@naver.com>
2025-05-13 15:42:19 +0900

    Feat/signup

---
+++ M8eat/src/main/java/com/prj/m8eat/model/dto/User.java
@@ -0,0 +1,42 @@
+package com.prj.m8eat.model.dto;
+
+public class User {
+	private int userNo;
+	private String name;
+	private String id;
+	private String password;
+	private String role;
+
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
