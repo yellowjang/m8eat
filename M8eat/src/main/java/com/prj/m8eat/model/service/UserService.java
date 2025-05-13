cf59482899360959905791f2f7b53f5111ddd7b3
김민정 <alswjd35791@naver.com>
2025-05-13 17:16:08 +0900

    Feat/login

--- M8eat/src/main/java/com/prj/m8eat/model/service/UserService.java
+++ M8eat/src/main/java/com/prj/m8eat/model/service/UserService.java
@@ -1,9 +1,12 @@
 package com.prj.m8eat.model.service;
 
+import com.prj.m8eat.model.dto.LoginResponse;
 import com.prj.m8eat.model.dto.User;
 
 public interface UserService {
 
 	public int signup(User user);
+
+	public LoginResponse login(User user);
 
 }
