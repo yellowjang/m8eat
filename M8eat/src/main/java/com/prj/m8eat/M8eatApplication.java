188a2bd15b8f67f011ae1eab7cc1b53f45c5f403
김민정 <alswjd35791@naver.com>
2025-05-13 12:55:20 +0900

    0513 projectCreate

---
+++ M8eat/src/main/java/com/prj/m8eat/M8eatApplication.java
@@ -0,0 +1,13 @@
+package com.prj.m8eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M8eatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M8eatApplication.class, args);
+	}
+
+}
