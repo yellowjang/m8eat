2472f13dd1720690ea80588864e3917338a9b7f1
김민정 <alswjd35791@naver.com>
2025-05-13 15:42:19 +0900

    Feat/signup

---
+++ M8eat/src/main/java/com/prj/m8eat/model/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.prj.m8eat.model.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.prj.m8eat.model.dto.User;
+
+@Mapper
+public interface UserDao {
+
+	public int insertUser(User user);
+
+}
