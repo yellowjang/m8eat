e48e5389877e727ecd443e3481c7f20303d47b5e
김민정 <alswjd35791@naver.com>
2025-05-15 15:46:11 +0900

    feat/signupInfo

--- M8eat/src/main/java/com/prj/m8eat/model/service/UserServiceImpl.java
+++ M8eat/src/main/java/com/prj/m8eat/model/service/UserServiceImpl.java
@@ -5,33 +5,39 @@ import org.springframework.stereotype.Service;
 import com.prj.m8eat.model.dao.UserDao;
 import com.prj.m8eat.model.dto.LoginResponse;
 import com.prj.m8eat.model.dto.User;
+import com.prj.m8eat.model.dto.UserHealthInfo;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	private final UserDao userDao;
+
 	public UserServiceImpl(UserDao userDao) {
 		this.userDao = userDao;
 	}
 
 	@Override
-	public int signup(User user) {
-		return userDao.insertUser(user);
+	public int signup(User user, UserHealthInfo healthInfo) {
+		if (userDao.insertUser(user) == 1) {
+			healthInfo.setUserNo(user.getUserNo());
+			return userDao.insertUserHealthInfo(healthInfo);
+		}
+		return 0;
 	}
 
 	@Override
 	public LoginResponse login(User user) {
 		User isUser = userDao.selectUser(user.getId());
-		if (isUser != null) {  // 존재하는 아이디
-			if (user.getPassword().equals(isUser.getPassword())) {  // 아이디 존재, 비밀번호 일치
+		if (isUser != null) { // 존재하는 아이디
+			if (user.getPassword().equals(isUser.getPassword())) { // 아이디 존재, 비밀번호 일치
 				return new LoginResponse(true, "로그인 되었습니다.", isUser.getId());
-			} else {  // 아이디 존재, 비밀번호 불일치
+			} else { // 아이디 존재, 비밀번호 불일치
 				return new LoginResponse(false, " 비밀번호가 일치하지 않습니다.", null);
 			}
-		} else {  // 존재하지 않는 아이디
+		} else { // 존재하지 않는 아이디
 			return new LoginResponse(false, "존재하지 않는 아이디입니다.", null);
 		}
-		
+
 	}
 
 	@Override
