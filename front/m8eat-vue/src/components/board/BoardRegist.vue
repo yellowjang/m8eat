658aa746517d27fd1bcea71c0ef6dfa05accdf1e
alswjd0216 <alswjd016@korea.ac.kr>
2025-05-24 17:22:16 +0900

    feat/게시글 등록 연동

--- front/m8eat-vue/src/components/board/BoardRegist.vue
+++ front/m8eat-vue/src/components/board/BoardRegist.vue
@@ -12,6 +12,12 @@
         <textarea v-model.trim="content" id="content" required></textarea>
       </div>
 
+      <!-- ✅ 파일 첨부 -->
+      <div class="form-group">
+        <label for="file">파일 첨부</label>
+        <input type="file" id="file" @change="onFileChange" />
+      </div>
+
       <button type="submit" class="submit-btn">등록</button>
     </form>
   </div>
@@ -20,24 +26,44 @@
 <script setup>
 import { ref, reactive } from "vue";
 import { addBoard } from "@/api/board";
+import { useBoardStore } from "@/stores/board";
+import { useRouter } from "vue-router";
+
+const store = useBoardStore();
+const router = useRouter();
 
 // const form = reactive({
 //   title: "",
 //   content: "",
 // });
 
-const title = ref('')
-const content = ref('')
+const title = ref("");
+const content = ref("");
+const file = ref(null);
+
+const onFileChange = (event) => {
+  file.value = event.target.files[0];
+};
 
 const submitForm = async () => {
   try {
-    await addBoard();
-    alert("게시글이 등록되었습니다.");
+    const formData = new FormData();
+    formData.append("title", title.value);
+    formData.append("content", content.value);
+
+    if (file.value) {
+      formData.append("file", file.value);
+    }
+
+    await store.addBoard(formData);
+    alert("게시글을 등록하였습니다.");
+    // alert("게시글이 등록되었습니다.");
     // TODO: 성공 후 이동 (예: 게시글 목록)
   } catch (error) {
     console.error(error);
     alert("등록 중 오류가 발생했습니다.");
   }
+  router.push({ name: "boardList" });
 };
 import "@/style/boardForm.scss";
 </script>
