18764eb8be1c970a2611f1152d248da1178ea6a6
alswjd0216 <alswjd016@korea.ac.kr>
2025-05-24 17:50:32 +0900

    feat/게시글 상세 화면 이미지, 파일까지

--- M8eat/src/main/java/com/prj/m8eat/config/WebConfig.java
+++ M8eat/src/main/java/com/prj/m8eat/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.prj.m8eat.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.prj.m8eat.interceptor.JwtInterceptor;
@@ -30,4 +32,14 @@ public class WebConfig implements WebMvcConfigurer {
 																"/css/**", "/js/**",
 																"/swagger-ui/**", "/v3/api-docs/**");
 	}
+	
+    @Value("${file.upload.dir}")
+    private String uploadPath; // 예: C:/Users/kmj/Desktop/SSAFY/imgs
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /upload/** 로 들어온 요청을 실제 디렉토리로 매핑
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:///" + uploadPath + "/");
+    }
 }
