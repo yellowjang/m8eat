b1240353d2bf6389783caf943ffaa750e442897e
alswjd0216 <alswjd016@korea.ac.kr>
2025-05-25 00:37:13 +0900

    feat/일대일 채팅 구현

---
+++ M8eat/src/main/java/com/prj/m8eat/model/dto/ChatMessage.java
@@ -0,0 +1,44 @@
+package com.prj.m8eat.model.dto;
+
+import java.time.LocalDateTime;
+
+public class ChatMessage {
+    private Long id;
+    private Long roomId;
+    private String sender;
+    private String content;
+    private LocalDateTime sentAt;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(Long roomId) {
+		this.roomId = roomId;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public LocalDateTime getSentAt() {
+		return sentAt;
+	}
+	public void setSentAt(LocalDateTime sentAt) {
+		this.sentAt = sentAt;
+	}
+    
+    
+}
--- M8eat/src/main/java/com/prj/m8eat/model/dto/ChatRoom.java
+++ M8eat/src/main/java/com/prj/m8eat/model/dto/ChatRoom.java
@@ -1,27 +1,65 @@
 package com.prj.m8eat.model.dto;
 
+import java.time.LocalDateTime;
+
 public class ChatRoom {
-	private int roomNo;
-	private String roomType;
-	private String createdAt;
-	public int getRoomNo() {
-		return roomNo;
+	
+	private Long id;
+	private String user1;
+    private String user2;
+    private LocalDateTime createdAt;
+    
+    
+	public Long getId() {
+		return id;
 	}
-	public void setRoomNo(int roomNo) {
-		this.roomNo = roomNo;
+	public void setId(Long id) {
+		this.id = id;
 	}
-	public String getRoomType() {
-		return roomType;
+	public String getUser1() {
+		return user1;
 	}
-	public void setRoomType(String roomType) {
-		this.roomType = roomType;
+	public void setUser1(String user1) {
+		this.user1 = user1;
 	}
-	public String getCreatedAt() {
+	public String getUser2() {
+		return user2;
+	}
+	public void setUser2(String user2) {
+		this.user2 = user2;
+	}
+	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
-	public void setCreatedAt(String createdAt) {
+	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
+    
+    
+    
+	
+	
+//	private int roomNo;
+//	private String roomType;
+//	private String createdAt;
+//	public int getRoomNo() {
+//		return roomNo;
+//	}
+//	public void setRoomNo(int roomNo) {
+//		this.roomNo = roomNo;
+//	}
+//	public String getRoomType() {
+//		return roomType;
+//	}
+//	public void setRoomType(String roomType) {
+//		this.roomType = roomType;
+//	}
+//	public String getCreatedAt() {
+//		return createdAt;
+//	}
+//	public void setCreatedAt(String createdAt) {
+//		this.createdAt = createdAt;
+//	}
 	
 	
 }
